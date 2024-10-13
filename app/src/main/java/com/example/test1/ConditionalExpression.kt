e6057fe8eba81aba2ceb17e18a8d262928bce3ad
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-10-13 16:06:23 +0700

    try-catch menangani pengecualian string ke interger, jika gagal invalid number. membuat fungsi perulangan  for loop 1 hingga 5, perulangan list, perulangan while, perulangan do dan while

--- app/src/main/java/com/example/test1/ConditionalExpression.kt
+++ app/src/main/java/com/example/test1/ConditionalExpression.kt
@@ -6,20 +6,20 @@ import java.util.concurrent.locks.Condition
 // Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
 // Conditional expression menggunakan if-else, when, dan try-catch
 
-fun ConditionalStatement(){
+fun ConditionalStatement() {
     println("=== Conditional Expression ===")
 
     // If-Else
     val number = 10
     if (number > 0) {
         println("Positive Number")
-    } else{
+    } else {
         println("Negative Number")
     }
 
     // When
     val day = 1
-    when (day){
+    when (day) {
         1 -> println("Sunday")
         2 -> println("Mondey")
         3 -> println("Tueday")
@@ -29,4 +29,41 @@ fun ConditionalStatement(){
         7 -> println("Saturday")
         else -> println("Invalid Day")
     }
+
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+
+    fun  Perulangan(){
+        println()
+        println("=== Perlualangan ===")
+
+        for (i in 1..5){
+            println("Perulangan ke~$i")
+        }
+
+        val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+        for (car in cars){
+            println("Car type: $car")
+        }
+
+        var x = 1
+        while (x <= 5) {
+            println("Perulangan ke-$x")
+            x++
+        }
+
+        var y = 1
+        do {
+            println("Perulangan ke-$y")
+            y++
+        }while (y <= 5 )
+    }
+
+
 }
