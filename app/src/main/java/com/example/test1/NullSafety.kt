2458ff76e3b6674dfbbcc1f27436e75ccc5e013c
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-10-13 18:04:45 +0700

    Penggunaan variable nevernull dan nullable, pengcheck null dengan if dan else, pemanggilan operator, fungsi strlength tidak boleh null

---
+++ app/src/main/java/com/example/test1/NullSafety.kt
@@ -0,0 +1,31 @@
+package com.example.test1
+
+fun nullSafety(){
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    // This is Ok
+    nullable = null
+
+    // Check for null in conditions
+    if (neverNull == null){
+        println("interredNonNull is null")
+    } else {
+        println("interredNonNull is not null")
+    }
+
+    // Safe call operator
+    println(neverNull.length)//18
+    println(nullable?.length) //null
+
+    fun strLength(notNull: String): Int{
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) // 18
+}
