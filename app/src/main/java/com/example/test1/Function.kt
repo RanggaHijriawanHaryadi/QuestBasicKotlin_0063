285838a696a547a1c05a2de513733a89142ef850
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-10-13 16:40:00 +0700

    fungsi tanpa parameter, dengan parameter name dijadikan string

---
+++ app/src/main/java/com/example/test1/Function.kt
@@ -0,0 +1,12 @@
+package com.example.test1
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
