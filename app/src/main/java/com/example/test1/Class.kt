8069487c47b62bf8f9178080306570b1537f264c
RanggaHijriawanHaryadi <127277495+RanggaHijriawanHaryadi@users.noreply.github.com>
2024-10-13 15:12:02 +0700

    function menjalankan dengan varible contact yang terdiri dari id dan email. juga dapat mengupdate email menjadi jane@gmail.com

--- app/src/main/java/com/example/test1/Class.kt
+++ app/src/main/java/com/example/test1/Class.kt
@@ -3,3 +3,16 @@ package com.example.test1
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Prints the value of the property: email
+    println(contact.email)
+
+    // Updates the values of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Prints the new values of the property: email
+    println(contact.email)
+}
